905482c0c162f9f001ca45f5f9092920e3ad3639
kdg9691 <rach26648@gmail.com>
2025-08-17 04:41:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kdg/",
+  base: "/kdg/", // 👈 this is important for GitHub Pages
 });
